56a3e4582385821452d5bc47e421a6dd72c3db06
Yuri Magalhaes <yurirobertocm@gmail.com>
2019-02-13 08:50:01 -0300

    Executando Consultas SQL

--- yii2-app-basic/controllers/DefaultController.php
+++ yii2-app-basic/controllers/DefaultController.php
@@ -2,7 +2,8 @@
 
 namespace app\controllers;
 
-use Yii; 
+use Yii;
+use yii\helpers\VarDumper; 
 use yii\web\Controller;
 
 class DefaultController extends Controller{
@@ -15,6 +16,30 @@ class DefaultController extends Controller{
 
             echo $company['id'] . " - " . $company['name'] . "<br>";
 
+        }
+
+    }
+
+    public function actionCompany(){
+
+        $company = Yii::$app->db->createCommand("select * from company where id = :id". [
+            ':id' => $id
+        ])->queryOne();
+
+        VarDumper::dump($company, 10, true);
+
+    }
+
+    public function actionJoin(){
+
+        $sql = "select p.nmae as product, p.release_date, c.name from product p
+            inner join company c on p.company_id = c.id
+        ";
+
+        $products = Yii::$app->db->createCommand($sql)->queryAll();
+
+        foreach ($products as $product){
+            echo $product['product'] . ", " . $product['release_date'] . ", " . $product['company'] . "<br>";
         }
 
     }
